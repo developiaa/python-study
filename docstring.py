7a7b90d08fca9f881769dd2f0da9e10b19493a38
developiaa <developiaa@gmail.com>
2021-04-22 23:26:10 +0900

    Add docstring example

---
+++ docstring.py
@@ -0,0 +1,20 @@
+class Docstring:
+    """
+    클래스 설명을 명시하는 부분
+    """
+
+    def docstring_fucntion(self):
+        """
+        함수의 설명을 명시하는 부분
+        """
+        pass
+
+
+
+# REPL (Read Evaluate Print Loop)
+
+# from docstring import Docstring
+# Docstring.__doc__ 속성이 추가됨
+
+# module의 docstring과 내부 모든 함수의 docstring을 확인 할 수 있다.
+# help(Docstring)
